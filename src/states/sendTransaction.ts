310d348beaf8e1fffd40e919486654233cf12e48
Nazeeh Vahora <nazeehvahora.786@gmail.com>
2022-10-19 22:16:23 +0530

    Complete eth transfer

--- src/states/sendTransaction.ts
+++ src/states/sendTransaction.ts
@@ -8,68 +8,66 @@ interface SendTransactionContext {
   amount: number;
 }
 
-export const sendTransactionMachine = createMachine<SendTransactionContext>(
-  {
-    id: "transaction",
-    initial: "idle",
-    context: {
-      res: null,
-      error: null,
-      address: "",
-      amount: 0,
-    },
-    states: {
-      idle: {
-        on: {
-          SEND_TRANSACTION: "loading",
-        },
-      },
-      loading: {
-        // @ts-expect-error
-        invoke: {
-          id: "sendTransaction",
-          src: sendTransaction,
-          onDone: {
-            target: "resolved",
-            actions: assign({
-              res: (_, event) => event.data,
-              address: "",
-              amount: 0,
-              error: null,
-            }),
-          },
-          onError: {
-            target: "rejected",
-            actions: assign({
-              error: (_, event) => event.data,
-            }),
-          },
-        },
-        on: {
-          CANCEL: "idle",
-        },
-      },
-      resolved: {
-        type: "final",
-      },
-      rejected: {
-        on: {
-          SEND_TRANSACTION: "loading",
-        },
+export const sendTransactionMachine = createMachine<SendTransactionContext>({
+  id: "transaction",
+  initial: "idle",
+  context: {
+    res: null,
+    error: null,
+    address: "",
+    amount: 0,
+  },
+  states: {
+    idle: {
+      on: {
+        SEND_TRANSACTION: "loading",
       },
     },
-    on: {
-      UPDATING_ADDRESS: {
-        actions: assign({
-          address: (_, event) => event.value,
-        }),
+    loading: {
+      // @ts-expect-error
+      invoke: {
+        id: "sendTransaction",
+        src: (context, event) =>
+          sendTransaction(context.address, context.amount.toString()),
+        onDone: {
+          target: "resolved",
+          actions: assign({
+            res: (_, event) => event.data,
+            address: "",
+            amount: 0,
+            error: null,
+          }),
+        },
+        onError: {
+          target: "rejected",
+          actions: assign({
+            error: (_, event) => event.data,
+          }),
+        },
       },
-      UPDATING_AMOUNT: {
-        actions: assign({
-          amount: (_, event) => event.value,
-        }),
+      on: {
+        CANCEL: "idle",
+      },
+    },
+    resolved: {
+      type: "final",
+    },
+    rejected: {
+      on: {
+        SEND_TRANSACTION: "loading",
       },
     },
   },
-  
-);
+  on: {
+    UPDATING_ADDRESS: {
+      actions: assign({
+        address: (_, event) => event.value,
+      }),
+    },
+    UPDATING_AMOUNT: {
+      actions: assign({
+        amount: (_, event) => event.value,
+      }),
+    },
+  },
+});
